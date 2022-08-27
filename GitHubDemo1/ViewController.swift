2b96a825e703cdaa68e5d972061b9d501263436d
Akramjon <akramjon00175@gmail.com>
2022-08-27 10:38:25 -0700

    Initial Commit

---
+++ GitHubDemo1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubDemo1
+//
+//  Created by Akramjon on 27/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
